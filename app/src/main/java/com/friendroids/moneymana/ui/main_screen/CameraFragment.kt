f7a6d98e5776ae4e42281fd59961933a6702770c
gorinih <gorinih@gmail.com>
2021-03-13 01:58:38 +0700

    add Camera preview start app and run camera preview

--- app/src/main/java/com/friendroids/moneymana/ui/main_screen/CameraFragment.kt
+++ app/src/main/java/com/friendroids/moneymana/ui/main_screen/CameraFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.camera.core.CameraX
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.friendroids.moneymana.data.camera.ManaCameraX
 import com.friendroids.moneymana.databinding.FragmentCameraBinding
+import java.util.concurrent.Executor
 
-class CameraFragment: Fragment() {
+class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var cameraX: ManaCameraX
+    private lateinit var executor: Executor
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +30,15 @@ class CameraFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        executor = ContextCompat.getMainExecutor(requireContext())
+        cameraX = ManaCameraX(requireContext(), executor, binding.cameraView, viewLifecycleOwner)
+        binding.button.setOnClickListener {
+            // takePhoto(this)
+        }
+        cameraX.bindCamera()
     }
 
-    companion object{
-
+    companion object {
         fun newInstance() = CameraFragment()
     }
 }
